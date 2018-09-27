5610af8a9c7a85383a6807b07b6fdcf30153d8f1
taoothereporter <33506655+taoothereporter@users.noreply.github.com>
2018-09-27 22:16:06 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,36 @@
 
 var canvas = new fabric.Canvas('c1');
 
+
+var canvas1 = new fabric.Canvas('c2');
+
+var composite = [];
+
+function addCircle() {
+    var circle = new fabric.Circle({
+        radius: 100,
+        fill: 'green'
+    });
+
+    composite.push(circle);
+
+    canvas1.add(circle);
+}
+
+function addRectangle() {
+    var rect = new fabric.Rect({
+        width: 40,
+        height: 40,
+        fill: 'red'
+    });
+
+    composite.push(rect);    
+
+    canvas1.add(rect);
+}
+
+
 var text = new fabric.Text('hello world', { left: 100, top: 100 });
-
-
 
 // canvas.setHeight("768");
 // canvas.setWidth("1024");
@@ -16,7 +43,7 @@ fabric.Image.fromURL(imgSource, function(oImg) {
     oImg.scaleToWidth(370);
     oImg.scaleToHeight(480);
     headline = oImg;
-    canvas.add(oImg);
+    canvas1.add(oImg);
 });
 
 var imgTshirt = 't-shirt.png';
@@ -36,7 +63,25 @@ fabric.Image.fromURL(imgTshirt, function(oImg) {
     oImg.center();
 });
 
-canvas.add(text);
+// canvas.add(text);
+
+var group = new fabric.Group();
+
+group.add(new fabric.Rect({ 
+    width: 20, 
+    height: 20, 
+    fill: 'yellow'
+}));
+
+group.addWithUpdate(new fabric.Rect({
+    width: 40,
+    height: 40,
+    fill: 'red',
+    left: group.get('left')+20,
+    top: group.get('top')+20
+}));
+
+// canvas.add(group);
 
 canvas.selectionColor = 'rgba(0,255,0,0.3)';
 canvas.selectionBorderColor = 'red';
@@ -45,7 +90,7 @@ canvas.selectionLineWidth = 5;
 
 function getSrc(img){
     console.log(img.src);
-    canvas.remove(headline);
+    // canvas.remove(headline);
 
     fabric.Image.fromURL(img.src, function(oImg) {
         oImg.top = headline.top;
@@ -56,8 +101,8 @@ function getSrc(img){
         // oImg.height = headline.get('height');
         // oImg.scaleX = headline.get('scaleX');
         // oImg.scaleY = headline.get('scaleY');
-        headline = oImg;
-        canvas.add(oImg);
+        // headline = oImg;
+        canvas1.add(oImg);
     });
 }
 
@@ -77,4 +122,54 @@ function getTshirt(img){
         oImg.center();
     });
     
+}
+
+function groupObjs() {
+    if (!canvas1.getActiveObject()) {
+        return;
+    }
+
+    if (canvas1.getActiveObject().type !== 'activeSelection') {
+        return;
+    }
+
+    canvas1.getActiveObject().toGroup();
+    canvas1.requestRenderAll();
+}
+
+function addToMain() {
+    // if (!canvas1.getActiveObject()) {
+    //     return;
+    // }
+
+    // if (canvas1.getActiveObject().type !== 'activeSelection') {
+    //     return;
+    // }
+
+    var item = canvas1.getActiveObject();
+    
+    canvas.add(item);
+}
+
+function deleteSelected(){
+    var selectedItem = canvas1.getActiveObject();
+
+    canvas1.remove(selectedItem);
+}
+
+function bringUp(){
+    var selectedItem1 = canvas1.getActiveObject();
+
+    canvas1.bringForward(selectedItem1);
+}
+
+function bringDown(){
+    var selectedItem2 = canvas1.getActiveObject();
+
+    canvas1.sendBackwards(selectedItem2);    
+}
+
+function clearCanvas() {
+    canvas1.clear();
+    canvas1.requestRenderAll();
 }
