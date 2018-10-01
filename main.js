d65d09c7956902d368bf883440cea1b6ce183523
taoothereporter <33506655+taoothereporter@users.noreply.github.com>
2018-10-01 07:51:35 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,175 +1,227 @@
-
-var canvas = new fabric.Canvas('c1');
-
-
-var canvas1 = new fabric.Canvas('c2');
-
-var composite = [];
-
-function addCircle() {
-    var circle = new fabric.Circle({
-        radius: 100,
-        fill: 'green'
-    });
-
-    composite.push(circle);
-
-    canvas1.add(circle);
-}
-
-function addRectangle() {
-    var rect = new fabric.Rect({
-        width: 40,
-        height: 40,
-        fill: 'red'
-    });
-
-    composite.push(rect);    
-
-    canvas1.add(rect);
-}
-
-
-var text = new fabric.Text('hello world', { left: 100, top: 100 });
-
-// canvas.setHeight("768");
-// canvas.setWidth("1024");
-
-var imgSource = 'title.png';
-
-var headline;
-
-fabric.Image.fromURL(imgSource, function(oImg) {
-    oImg.scaleToWidth(370);
-    oImg.scaleToHeight(480);
-    headline = oImg;
-    canvas1.add(oImg);
+var canvas = this.__canvas = new fabric.Canvas('c1', {
+    // isDrawingMode: true
 });
 
-var imgTshirt = 't-shirt.png';
+var clipPoly = new fabric.Polygon([
+    { x: 180, y: 10 },
+    { x: 300, y: 50 },
+    { x: 300, y: 180 },
+    { x: 180, y: 220 }
+ ], {
+    originX: 'left',
+    originY: 'top',
+    left: 180,
+    top: 10,
+    width: 200,
+    height: 200,
+    fill: '#ddd', /* use transparent for no fill */
+    strokeWidth: 0
+});
 
-var origTshirt;
-
-fabric.Image.fromURL(imgTshirt, function(oImg) {
-    oImg.lockMovementX = true;
-    oImg.lockMovementY = true;
+fabric.Image.fromURL('t-shirt.png', function (oImg) {
+    oImg.id = 'fixed';
+    oImg.scaleToWidth(570);
+    oImg.scaleToHeight(680);
     oImg.selectable = false;
-    oImg.hasControls = false;
-    oImg.scaleToWidth(540);
-    oImg.scaleToHeight(760);
-    origTshirt = oImg;
+
     canvas.add(oImg);
-    canvas.sendBackwards(oImg);
-    oImg.center();
+    
+    // canvas.add(clipPoly);
 });
 
-// canvas.add(text);
+$('#addImageModalDialog').on('click',function (){
+    $('.modal').addClass('is-active');
+});
 
-var group = new fabric.Group();
+$('#closeImageModalDialog').on('click',function (){
+    $('#customImageURL').val('');
+    $('.modal').removeClass('is-active');
+});
 
-group.add(new fabric.Rect({ 
-    width: 20, 
-    height: 20, 
-    fill: 'yellow'
-}));
+$('#cancelImageModalDialog').on('click',function (){
+    $('#customImageURL').val('');
+    $('.modal').removeClass('is-active');
+});
 
-group.addWithUpdate(new fabric.Rect({
-    width: 40,
-    height: 40,
-    fill: 'red',
-    left: group.get('left')+20,
-    top: group.get('top')+20
-}));
+$('#addImageFromURL').on('click',function (){
 
-// canvas.add(group);
+    fabric.Image.fromURL($('#customImageURL').val(), function (oImg) {
+        oImg.set({
+            clipTo: function (ctx) {
+                if (clipPoly) {
+                    this.setCoords();
+                    var scaleXTo1 = (1 / this.scaleX);
+                    var scaleYTo1 = (1 / this.scaleY);
+                    ctx.save();
 
-canvas.selectionColor = 'rgba(0,255,0,0.3)';
-canvas.selectionBorderColor = 'red';
-canvas.selectionLineWidth = 5;
-// this._canvases.push(canvas);
+                    var ctxLeft = -( this.width / 2 ) + clipPoly.strokeWidth;
+                    var ctxTop = -( this.height / 2 ) + clipPoly.strokeWidth;
+                    var ctxWidth = clipPoly.width - clipPoly.strokeWidth;
+                    var ctxHeight = clipPoly.height - clipPoly.strokeWidth;
 
-function getSrc(img){
-    console.log(img.src);
-    // canvas.remove(headline);
+                    ctx.translate( ctxLeft, ctxTop );
+                    ctx.scale(scaleXTo1, scaleYTo1);
+                    ctx.rotate(0);
 
-    fabric.Image.fromURL(img.src, function(oImg) {
-        oImg.top = headline.top;
-        oImg.left = headline.left;
-        oImg.scaleToWidth(headline.width*headline.scaleX);
-        oImg.scaleToHeight(headline.height*headline.scaleY);
-        // oImg.width = headline.get('width');
-        // oImg.height = headline.get('height');
-        // oImg.scaleX = headline.get('scaleX');
-        // oImg.scaleY = headline.get('scaleY');
-        // headline = oImg;
-        canvas1.add(oImg);
-    });
-}
+                    ctx.beginPath();
 
-function getTshirt(img){
-    canvas.remove(origTshirt);
+                    var points = [];
 
-    fabric.Image.fromURL(img.src, function(oImg) {
-        oImg.lockMovementX = true;
-        oImg.lockMovementY = true;
-        oImg.selectable = false;
-        oImg.hasControls = false;
-        oImg.scaleToWidth(540);
-        oImg.scaleToHeight(760);
-        origTshirt = oImg;
+                    for(i in clipPoly.points) {
+                        // points.push({
+                        //     x: (clipPoly.left + clipPoly.width / 2) + clipPoly.points[i].x - this.oCoords.tl.x,
+                        //     y: (clipPoly.top + clipPoly.height / 2) + clipPoly.points[i].y - this.oCoords.tl.y
+                        // });
+                        points.push({
+                            x: clipPoly.points[i].x - this.oCoords.tl.x,
+                            y: clipPoly.points[i].y - this.oCoords.tl.y
+                        });
+                    }
+
+                    ctx.moveTo(points[0].x, points[0].y);
+                    for(i=1; i<points.length; ++i) {
+                        ctx.lineTo(points[i].x, points[i].y);
+                    }
+                    ctx.lineTo(points[0].x, points[0].y);
+                    ctx.closePath();
+                    ctx.restore();
+                }                
+            }
+        });
         canvas.add(oImg);
-        canvas.sendBackwards(oImg);
-        oImg.center();
     });
-    
-}
 
-function groupObjs() {
-    if (!canvas1.getActiveObject()) {
-        return;
+    $('#customImageURL').val('');
+    $('.modal').removeClass('is-active');
+});
+
+var startDrawingPolygon = false;
+var polygonCount = 1;
+var ArrayLength;
+var circleCount = 1;
+// var fillColor = "rgba(46, 240, 56, 0.5)";
+var fillColor = "transparent";
+
+$('#drawPolygon').on('click', function () {
+    startDrawingPolygon = !startDrawingPolygon;
+
+    if (!startDrawingPolygon)
+    {
+        $('#drawPolygon').removeClass('is-danger');
+        canvas.remove(clipPoly);
+        ArrayLength = circleCount;
+        circleCount = 1;
+        var tempCount = 0;
+        var objects = canvas.getObjects();
+        var points = [];
+
+        for (var i = 0; objects.length > i; i++) {
+            if (objects[i].polygonNo === polygonCount) {
+                points.push({
+                    x: objects[i].left,
+                    y: objects[i].top
+                });
+                canvas.renderAll();
+            }
+        }
+
+        clipPoly = new fabric.Polygon(points, {
+            fill: fillColor,
+            PolygonNumber: polygonCount,
+            name: "Polygon",
+            selectable: false,
+            noofcircles: ArrayLength,
+            objectCaching: false
+        });
+
+        canvas.add(clipPoly);
+        // canvas.sendToBack(clipPoly)
+        canvas.renderAll();
+        polygonCount++;
     }
-
-    if (canvas1.getActiveObject().type !== 'activeSelection') {
-        return;
+    else {
+        $('#drawPolygon').addClass('is-danger');
     }
+});
 
-    canvas1.getActiveObject().toGroup();
-    canvas1.requestRenderAll();
-}
+canvas.on('object:moving', function(option) {
+    var object = option.target;
+    canvas.forEachObject(function(obj) {
+        if (obj.name == "Polygon") {
 
-function addToMain() {
-    // if (!canvas1.getActiveObject()) {
-    //     return;
-    // }
+            if (obj.PolygonNumber == object.polygonNo) {
+                var points = clipPoly.get("points");
+                points[object.circleNo - 1].x = object.left;
+                points[object.circleNo - 1].y = object.top;
 
-    // if (canvas1.getActiveObject().type !== 'activeSelection') {
-    //     return;
-    // }
+                clipPoly.set({
+                    points: points
+                });
+            }
 
-    var item = canvas1.getActiveObject();
-    
-    canvas.add(item);
-}
+        }
+    });
 
-function deleteSelected(){
-    var selectedItem = canvas1.getActiveObject();
+    canvas.renderAll();
+});
 
-    canvas1.remove(selectedItem);
-}
+canvas.on('mouse:down', function(option) {
+    if (option.target && option.target.name == "draggableCircle") {
+        return;
+    } else {
+        if (startDrawingPolygon) {
+            var pointer = canvas.getPointer(option.e);
 
-function bringUp(){
-    var selectedItem1 = canvas1.getActiveObject();
+            circle = new fabric.Circle({
+                left: pointer.x,
+                top: pointer.y,
+                radius: 7,
+                hasBorders: false,
+                hasControls: false,
+                polygonNo: polygonCount,
+                name: "draggableCircle",
+                circleNo: circleCount,
+                fill: "rgba(0, 0, 0, 0.5)",
+                hasRotatingPoint: false,
+                originX: 'center',
+                originY: 'center'
+            });
 
-    canvas1.bringForward(selectedItem1);
-}
+            canvas.add(circle);
+            circleCount++;
+        }
+    }
+});
 
-function bringDown(){
-    var selectedItem2 = canvas1.getActiveObject();
+$('#removeSelectedObject').on('click', function () {
+    canvas.remove(canvas.getActiveObject());
+});
 
-    canvas1.sendBackwards(selectedItem2);    
-}
+$('#clearViewport').on('click', function () {
+    canvas.getObjects().map(function (o) {
+        if (o.get('id') !== 'fixed') {
+            canvas.remove(o);
+        }
+    });
+});
 
-function clearCanvas() {
-    canvas1.clear();
-    canvas1.requestRenderAll();
-}
+$('#setColor').spectrum({
+    // color: "#f00",
+    change: function (tinycolor) {
+        console.log(tinycolor);
+        canvas.getObjects().map(function (o) {
+            if (o.get('id') === 'fixed') {
+                o.filters = [];
+                o.filters.push(new fabric.Image.filters.BlendColor({
+                    color: tinycolor.toHexString(),
+                    mode: 'overlay',
+                    alpha: 0.55
+                }));
+                o.applyFilters();
+            }
+        });
+        
+        canvas.renderAll();
+    }
+});
+
